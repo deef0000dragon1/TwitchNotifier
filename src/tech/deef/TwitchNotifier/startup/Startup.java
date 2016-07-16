1fc160f9d9cd0b3846db4635a108bfebee6d7db4
deef <deef551@gmail.com>
2016-07-16 15:41:08 -0500

    Add JSON testing classes. NOT FOR RELEASE USE

--- src/tech/deef/TwitchNotifier/startup/Startup.java
+++ src/tech/deef/TwitchNotifier/startup/Startup.java
@@ -8,31 +8,35 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import tech.deef.TwitchNotifier.live.ChannelStatus;
-
+import tech.deef.TwitchNotifier.testing.JsonReaderTest;
 
 /**
- * main class to test and deploy.
- * Curently set up using hard coded names.
+ * main class to test and deploy. Curently set up using hard coded names.
  */
 public class Startup extends JFrame {
-
 
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		Startup outline = new Startup();
-		outline.createGui();
-		while (true) {
-			outline.createGui();
-			try {
-				Thread.sleep(1000 * 60);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// setVisible(false);
-		}
+		boolean testing = true;
 
+		if (testing) {
+		JsonReaderTest.TestJsonReader();
+		}
+		else {
+			Startup outline = new Startup();
+			outline.createGui();
+			while (true) {
+				outline.createGui();
+				try {
+					Thread.sleep(1000 * 60);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// setVisible(false);
+			}
+		}
 	}
 
 	public Startup() {
@@ -40,15 +44,14 @@ public class Startup extends JFrame {
 	}
 
 	private void createGui() {
-		//main pannel that holds everything
+		// main pannel that holds everything
 		JPanel mainPanel = new JPanel();
 
-		//lists of the names and channels corosponding to those names
+		// lists of the names and channels corosponding to those names
 		ArrayList<String> channelNames = new ArrayList<String>();
 		ArrayList<ChannelStatus> channelInfo = new ArrayList<ChannelStatus>();
 
-		
-		//hard coded names for testing.
+		// hard coded names for testing.
 		channelNames.add("protonjon");
 		channelNames.add("skrumpie");
 		channelNames.add("geoff");
@@ -58,30 +61,31 @@ public class Startup extends JFrame {
 		channelNames.add("linustech");
 		channelNames.add("gamesdonequick");
 
-		//set gridlayput as a verticle list. 
+		// set gridlayput as a verticle list.
 		GridLayout mainLayout = new GridLayout(channelNames.size(), 1);
 
-		//use name from list of names to create a channel and add to list of channels
+		// use name from list of names to create a channel and add to list of
+		// channels
 		for (String nameOfChannel : channelNames) {
 			channelInfo.add(new ChannelStatus(nameOfChannel));
 		}
 
 		for (ChannelStatus channel : channelInfo) {
-			//create the gui layout for the panel. Not effective to what I need.
-			//replacing with a more effective method. 
-			//basic problem can be seen with the height of offline channels 
-			//vs the height of online ones. 
-			//also a problem is that online channels are not at the top.
+			// create the gui layout for the panel. Not effective to what I
+			// need.
+			// replacing with a more effective method.
+			// basic problem can be seen with the height of offline channels
+			// vs the height of online ones.
+			// also a problem is that online channels are not at the top.
 			GridBagConstraints constraints = new GridBagConstraints();
 			constraints.weighty = (channel.isLive() ? 200 : 0);
 			System.out.println(channel.isLive() + "   " + channel.getUserName());
 			mainPanel.add(channel.getPanel(), constraints);
 		}
 
-		
 		mainPanel.setLayout(mainLayout);
-		
-		setSize(800, 75*channelNames.size());
+
+		setSize(800, 75 * channelNames.size());
 		add(mainPanel);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
---
+++ src/tech/deef/TwitchNotifier/testing/JsonReaderTest.java
@@ -0,0 +1,54 @@
+package tech.deef.TwitchNotifier.testing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * This class is designed to test a JSONReader class and determine if it will
+ * be usable for the Twitch Reader This  is for specific scenario and
+ * developer testing It will have little to no error handling.
+ */
+public class JsonReaderTest {
+
+	/*
+	 * Test the JSON Reader
+	 */
+	public static void TestJsonReader() {
+		
+		
+	}
+
+	public static String getFollowerData() {
+
+		URL url;
+		InputStream inpusStream = null;
+		BufferedReader bufferedReader;
+		String data = null;
+
+		try {
+			url = new URL("https://api.twitch.tv/kraken/users/deef0000dragon1/follows/channels");
+			inpusStream = url.openStream(); // throws an IOException
+			bufferedReader = new BufferedReader(new InputStreamReader(inpusStream));
+
+			data = bufferedReader.readLine();
+		} catch (MalformedURLException mue) {
+			mue.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (inpusStream != null)
+					inpusStream.close();
+			} catch (IOException ioe) {
+				// nothing to see here
+			}
+		}
+		return data;
+	}
+
+}
